570199d67f279aedb84bcef428500fa23cc61d45
Will Harris <Harriswill22@yahoo.com>
2018-11-30 09:24:05 -0500

    adds the zero badge and increment button

--- src/compnents/counter.jsx
+++ src/compnents/counter.jsx
@@ -1,8 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
+    state = {
+        count: 0
+    };
+    styles = {
+        fontSize: 40,
+        fontWeight:"bold"
+    };
+
+    render() {
+        return (
+            <div>
+            <span style={{fontSize:30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
+    }
+    formatCount() {
+        const{ count } = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
 
