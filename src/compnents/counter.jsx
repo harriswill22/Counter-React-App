e2318aaa0ce0e882ea5bce6b5379ecf50be93995
Will Harris <Harriswill22@yahoo.com>
2018-11-30 12:34:15 -0500

    adds tags

--- src/compnents/counter.jsx
+++ src/compnents/counter.jsx
@@ -2,25 +2,32 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0
-    };
-    styles = {
-        fontSize: 40,
-        fontWeight:"bold"
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
-    render() {
-        return (
+    renderTags() {
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+        return  <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+
+    render(){
+            return (
             <div>
-            <span style={{fontSize:30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
+            {this.renderTags()};
             </div>
         );
     }
-    formatCount() {
-        const{ count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
+    // getBadgeClasses() {
+    //     let classes = "badge m-2 badge-";
+    //     classes += (this.state.count === 0) ? "warning" : "primary";
+    //     return classes;
+    // }
+
+    // formatCount() {
+    //     const{ count } = this.state;
+    //     return count === 0 ? "Zero" : count;
+    // }
 }
 
 export default Counter;
