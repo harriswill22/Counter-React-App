db9aac01c26db61fe3a7587285ce45469d3e0cd0
Will Harris <Harriswill22@yahoo.com>
2018-12-06 08:26:18 -0500

    updates index with App.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter'
+import Counter from './components/counter';
+import Counters from './components/counters'
 
-ReactDOM.render( < Counter / > , document.getElementById('root'));
+ReactDOM.render( < Counters / > , document.getElementById('root'));
+
 
