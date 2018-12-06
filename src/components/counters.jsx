3b7aef51db69a68a5d92ccedae92f94b1eb5082f
Will Harris <Harriswill22@yahoo.com>
2018-12-06 08:25:46 -0500

    passing data to components with props

---
+++ src/components/counters.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Counter from "./counter"
+
+class Counters  extends Component {
+    state = { 
+        counters: [
+            {id: 1, value: 0 },
+            {id: 2, value: 0 },
+            {id: 3, value: 0 },
+            {id: 4, value: 0 },
+            
+        ]
+    }
+
+_handleDelete = () =>{
+    console.log('Event Handler Called ');
+    
+}
+
+
+
+    render() { 
+        return ( 
+            <div>
+                {this.state.counters.map(counter => (
+                <Counter key={counter.id} value={counter.value}>
+                
+                </Counter>))}
+            </div>
+        );
+    }
+}
+ 
+export default  Counters;
