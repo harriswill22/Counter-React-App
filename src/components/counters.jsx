c158cee20b94a27b079007d2eae6c2e1abe01e4a
Will Harris <Harriswill22@yahoo.com>
2018-12-08 16:21:18 -0500

    adds reset button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -17,11 +17,20 @@ _handleDelete = counterId => {
     this.setState({counters})
 };
 
-
+_handleRest = () =>{
+   const counters = this.setState.map(c =>{
+        c.value =0;
+        return c;
+    });
+    this.setState({counters});
+}
 
     render() { 
         return ( 
             <div>
+                <button
+                onClick={this.handleRest}
+                className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => (
                 <Counter 
                 key={counter.id} 
